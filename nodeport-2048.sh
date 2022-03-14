65079c2c375ab1ec7e0bbbd633902b2bfe6d580f
abrarcv170 <78675575+abrarcv170@users.noreply.github.com>
2022-03-14 19:46:52 +0530

    Update nodeport-2048.sh

--- nodeport-2048.sh
+++ nodeport-2048.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-podname=$(kubectl get pod -n gameo  |  awk 'FNR ==2 {print $1}')
-hostip=$(kubectl get  pod $podname -n gameo -o  custom-columns=NAME:status.hostIP | awk 'FNR ==2 {print $1}')
-nodeport=$(kubectl get svc game-2048  -n gameo  -o custom-columns=NAME:spec.ports[0].nodePort | awk 'FNR ==2 {print $1}')
+podname=$(kubectl get pod -n game  |  awk 'FNR ==2 {print $1}')
+hostip=$(kubectl get  pod $podname -n game -o  custom-columns=NAME:status.hostIP | awk 'FNR ==2 {print $1}')
+nodeport=$(kubectl get svc game-2048  -n game  -o custom-columns=NAME:spec.ports[0].nodePort | awk 'FNR ==2 {print $1}')
 echo "connect using http://${hostip}:${nodeport}"
